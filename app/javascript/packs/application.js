2852ba8017696e91de374b56b8b2454d79a05614
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-08-18 12:30:39 +0200

    CV set à fond

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("plugins/animation.js")
+require("jquery") // Don't really need to require this...
+require("jquery-ui")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
