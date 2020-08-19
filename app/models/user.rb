d21e6741ffec4e2c9125466f19637d160c2d311e
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-08-19 23:25:41 +0200

    Admin resources create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :experiences
+  has_many :educations
+  has_many :skills
+  has_many :realisations
+
 end
