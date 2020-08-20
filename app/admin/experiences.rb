bff6a3abf1bc81d371e7aeb5c9cec2770657314f
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-08-20 18:39:16 +0200

    experience partial and model push forward

--- app/admin/experiences.rb
+++ app/admin/experiences.rb
@@ -3,18 +3,25 @@ ActiveAdmin.register Experience do
     belongs_to :experience
   end
   permit_params :entreprise, :start, :end, :role, :user_id, xp_description_attributes: [:id, :text, :item, :_destroy]
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:entreprise, :start, :end, :role, :user_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 
+  form do |f|
+    f.inputs do
+      f.input :user_id, :label => 'User', :as => :select, :collection => User.all.map{|u| ["#{u.first_name}", u.id]}
+      f.input :role
+      f.input :entreprise
+      f.inputs do
+        f.has_many :xp_description, allow_destroy: true do |t|
+          t.input :text
+        end
+      end
+      f.inputs do
+        f.has_many :xp_description, allow_destroy: true do |t|
+          t.input :item
+        end
+      end
+      f.input :start
+      f.input :end
+      f.actions
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   before_action :find_user, only: [ :show ]
   def show
     @experiences = Experience.where(user_id: @user.id)
-    # @experience = Experience.find(params[:id])
-    # @xp_descriptions = XpDescription.where(experience_id: @experience.id)
+    @experience = Experience.find(params[:id])
+    @xp_descriptions = XpDescription.where(experience_id: @experience.id)
     @educations = Education.where(user_id: @user.id)
     @skills = Skill.where(user_id: @user.id)
     @realisations = Realisation.where(user_id: @user.id)
