bef3d58cde402c2e2224c18cdf95599aeedf3dee
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-08-21 12:43:25 +0200

    problème enfin reglé

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,12 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [ :show ]
   def show
-    @experiences = Experience.where(user_id: @user.id)
-    @experiences.map do |exp|
-      @experience = exp
+    @experiences = @user.experiences
+    @xp_descriptions = @experiences.map(&:xp_descriptions)
+    @xp_descriptions.map do |description|
+      @xp_description = description
+      @xp_items = @xp_description.map(&:xp_items)
     end
-    @xp_descriptions = XpDescription.where(experience_id: @experience.id)
-    # @experience = Experience.find(params[:id])
-    # @experiences.each do |exp|
-    #   @xp_descriptions = exp.xp_descriptions
-    # end
-    # @xp_descriptions.each do |descro|
-    #   @xp_items = descro.xp_items
-    # end
-
-    # @xp_items = XpItem.where(xp_description_id: @xp_description.id)
-    # @xp_items.each do |xp_item|
-    #   @xp_item = xp_item
-    # end
     @educations = Education.where(user_id: @user.id)
     @skills = Skill.where(user_id: @user.id)
     @realisations = Realisation.where(user_id: @user.id)
@@ -30,3 +19,4 @@ private
   def find_user
     @user = User.find(params[:id])
   end
+
